a794b8c79d928ce95f83a5eeb0cfaadc298fc1b7
byoso <emailvinz@gmail.com>
2022-04-29 17:43:01 +0200

    minor improvement

--- silly_db/plop/db/database.py
+++ silly_db/plop/db/database.py
@@ -1,8 +1,12 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+"""This module is where the database is instantiated"""
 
 from silly_db.db import DB
+import os
 
+# Sets the .sqlit3 file alongside this database.py file
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-db = DB(file='my_db.sqlite3')
+db = DB(file=os.path.join(BASE_DIR, 'my_db.sqlite3'))
